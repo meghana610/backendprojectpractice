68ad6ab44932f0cb428e685d5e5e6765dbded7d5
meghana610 <devatameghana610@gmail.com>
2025-08-12 19:37:59 +0530

    practice steps of backend

---
+++ server/one.js
@@ -0,0 +1,21 @@
+console.log("This is Meghana")
+
+let fname = "Rama"
+let lname = "Meghana"
+
+console.log(fname+lname);
+
+let a = 25;
+let b = "Meghana"
+let c = false
+let d = null;
+
+
+let person ={
+    fname:"Meghana",
+    isAlive:true,
+    college:null
+}
+
+let students = ["teju","harshi","maggie"]
+console.log("students")
